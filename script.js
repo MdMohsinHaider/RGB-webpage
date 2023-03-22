e10b190ee4f87039543195949fc9ae8eb551b85e
Mohsin <mdmohsinhaider6@gmail.com>
2023-03-22 09:21:40 +0530

    .js file include

---
+++ script.js
@@ -0,0 +1,7 @@
+function data_get(){
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    
+    document.write("user name is "+username+"<br>");
+    document.write("your password is "+password+"<br>");
+}
